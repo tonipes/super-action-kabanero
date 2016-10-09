4a8f114fe3401bccd0488f2da77e58ae446ecdac
Toni <pesola.toni@gmail.com>
2016-10-09 13:10:05 +0300

    :wrench: Remove selene
    Because it is shit.

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,4 @@
 #include <iostream>
-#include <selene.h>
 #include "Dog.hpp"
 #include "kabanero/collection/mutable/Seq.hpp"
 #include "kabanero/collection/mutable/Map.hpp"
@@ -7,17 +6,6 @@
 
 int main() {
   auto dog = kabanero::Dog("Doge");
-
-  sel::State state;
-  state.Load("resources/script/test.lua");
-
-  auto foo = state["foo"];
-  auto bar_3 = state["bar"][3];
-  auto bar_key = state["bar"]["key"];
-
-  std::cout << int(foo) << std::endl;
-  std::cout << std::to_string(bar_3 == "asas") << std::endl;
-  std::cout << std::to_string(bar_key == "there") << std::endl;
 
   std::cout << "Dog is " << dog.name << std::endl;
   dog.Woof();
@@ -99,6 +87,9 @@ int main() {
   dogs += Dog("Emma");
   dogs += Dog("Döge");
   dogs += Dog("Musti");
+
+  VectorSeq<Dog> initdogs = { Dog("Taneli"), Dog("Liisa"), Dog("Eino") };
+  std::cout << "Initialize dogs: " << initdogs << std::endl;
 
   std::cout << dogs << std::endl;
   dogs.foreach([](auto& d){ d.name += " II"; });
