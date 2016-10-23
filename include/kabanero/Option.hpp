42ea45b7b22febdbdb1ce363f6e35527c31c90c0
Juuso Toikka <jtoikka@gmail.com>
2016-10-24 02:09:44 +0300

    :package: Make Option use shared_ptr internally
    This enables the Option to take a wider array of types without running
    into the problem of objects being removed from the stack and no longer
    existing. Additionally, now shared pointers can be wrapped in an Option
    type (function Some(shared_ptr<T>) will create an Option<T> instance).

--- include/kabanero/Option.hpp
+++ include/kabanero/Option.hpp
@@ -1,33 +1,37 @@
 #pragma once
 
 #include <iostream>
+#include <vector>
 
 template <typename T>
 class Option {
 public:
-  Option(T& value) : valuePtr(&value) {}
-  Option(const T& value) : valuePtr(&value) {}
+  Option(std::shared_ptr<T> valuePtr) : _isDefined(true), _valueStore(valuePtr) {}
 
-  Option() : valuePtr(nullptr) {}
+  Option(T value) : _isDefined(true) {
+    _valueStore = std::make_shared<T>(value);
+  }
 
-  // Now you can `if(opt)`. This equals to `if(opt.isDefined)`
+  Option() {}
+
+  // `if(opt)` equals `if(opt.isDefined)`
   operator bool() const { return isDefined(); }
 
   auto isDefined() const -> bool {
-    return valuePtr != nullptr;
+    return _isDefined;
   }
 
   auto isEmpty() const -> bool {
-    return valuePtr == nullptr;
+    return !_isDefined;
   }
 
-  auto get() const -> const T& {
-    return *valuePtr;
+  auto get() const -> const T {
+    return *_valueStore;
   }
 
   auto getOrElse(const T& v) const -> const T& {
-    if (valuePtr != nullptr) {
-      return *valuePtr;
+    if (isDefined()) {
+      return *_valueStore;
     } else {
       return v;
     }
@@ -36,15 +40,23 @@ public:
   template <typename F, typename R = typename std::result_of<F&(T)>::type>
   auto map(F func) const -> const Option<R> {
     if (isDefined()) {
-      return Option<R>(func(*valuePtr));
+      return Option<R>(func(*_valueStore));
     } else {
       return Option<R>();
     }
   }
 
+
+
 private:
-  const T* valuePtr;
+  std::shared_ptr<T> _valueStore;
+  bool _isDefined = false;
 };
+
+template <typename T>
+auto Some(std::shared_ptr<T>& value) -> Option<T> {
+  return Option<T>(value);
+}
 
 template <typename T>
 auto Some(T& value) -> Option<T> {
@@ -55,6 +67,12 @@ template <typename T>
 auto Some(const T& value) -> Option<T> {
   return Option<T>(value);
 }
+
+template <typename T>
+auto Some(const T value[]) -> Option<const T*> {
+  return Option<const T*>(&value[0]);
+}
+
 
 template <typename T>
 auto operator<<(std::ostream& os, const Option<T>& op) -> std::ostream& {
