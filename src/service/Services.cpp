ff6e7570b4dad552a648687ddda434f35dac43d0
Juuso Toikka <jtoikka@gmail.com>
2016-11-23 14:25:30 +0200

    :package: Move Services null set to cpp

---
+++ src/service/Services.cpp
@@ -0,0 +1,11 @@
+#include "service/Services.hpp"
+
+// Set messagePublisher default to NullMessagePublisher
+std::shared_ptr<MessagePublisher> Services::_messagePublisher
+  = std::make_shared<NullMessagePublisher>();
+// Set resourceManager default to NullResourceManager
+std::shared_ptr<ResourceManager> Services::_resourceManager
+  = std::make_shared<NullResourceManager>();
+// Set logger default to NullLogger
+std::shared_ptr<Logger> Services::_logger
+  = std::make_shared<NullLogger>();
