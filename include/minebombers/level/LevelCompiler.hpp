d7b7c88b6ef21309b3747dfcf08e27c7fb140120
Tuomas Reunanen <tuomas.reunanen@aalto.fi>
2016-11-24 21:54:58 +0200

    :construction: Added a really simple level-compiler, which takes a tilemap and converts most of it into nodes with only sprites in them

--- include/minebombers/level/LevelCompiler.hpp
+++ include/minebombers/level/LevelCompiler.hpp
@@ -4,18 +4,48 @@
 
 class LevelCompiler {
 public:
-  LevelCompiler(int tileSize) : _tileSize(tileSize) {}
-  auto materialize(TileMap& map, std::shared_ptr<Node<Transform3D>> rootNode) -> void {
+  LevelCompiler(Random& rand): _rand(rand) {}
+  auto materializeGround(TileMap& map) -> std::shared_ptr<Node<Transform3D>> {
     auto ground = std::make_shared<Node<Transform3D>>("ground");
+    for (auto x = 0; x < map.getWidth(); x++) {
+      for (auto y = 0; y < map.getHeight(); y++) {
+        std::ostringstream oss;
+        oss << "ground" << x << "-" << y;
+        auto node = std::make_shared<Node<Transform3D>>(oss.str());
+        node->addAttachment(getSprite("tiles/dirt", 2));
+        node->setLocalPosition(glm::vec3(x, y, 0));
+        ground->addChild(node);
+      }
+    }
+    return ground;
+  }
+  auto materializeObjects(TileMap& map) -> std::shared_ptr<Node<Transform3D>> {
     auto objects = std::make_shared<Node<Transform3D>>("objects");
     for (auto x = 0; x < map.getWidth(); x++) {
       for (auto y = 0; y < map.getHeight(); y++) {
-        auto node = std::make_shared<Node<Transform3D>>("ground-" + x + "/" + y);
-        node->addAttachment(std::make_shared<SpriteAttachment>("tiles/dirt0"))
+        std::ostringstream oss;
+        oss << "wall" << x << "-" << y;
+        auto node = std::make_shared<Node<Transform3D>>(oss.str());
         node->setLocalPosition(glm::vec3(x, y, 0));
+        if (map[x][y].getType() == CAVE_WALL) {
+          node->addAttachment(getSprite("tiles/pebble_brown", 8));
+        } else if (map[x][y].getType() == INDESCTRUCTIBLE_WALL) {
+          node->addAttachment(getSprite("tiles/stone_brick", 11));
+        } else if (map[x][y].getType() == CONSTRUCTED_WALL) {
+          node->addAttachment(getSprite("tiles/rect_gray", 3));
+        } else if (map[x][y].getType() == WINDOW) {
+          node->addAttachment(getSprite("tiles/window", 0));
+        }
+        objects->addChild(node);
       }
     }
+    return objects;
   }
 private:
-  int _tileSize;
-}
+  Random& _rand;
+  auto getSprite(std::string baseName, int variations) -> std::shared_ptr<SpriteAttachment> {
+      std::ostringstream oss;
+      oss << baseName << _rand.nextInt(variations);
+      return std::make_shared<SpriteAttachment>(oss.str());
+  }
+};
--- src/minebombers/Minebombers.cpp
+++ src/minebombers/Minebombers.cpp
@@ -3,6 +3,8 @@
 #include "service/Services.hpp"
 #include "message/event/AudioClipEvent.hpp"
 #include "message/event/AudioTrackEvent.hpp"
+#include "minebombers/level/LevelCompiler.hpp"
+#include "minebombers/level/CaveGenerator.hpp"
 
 auto Minebombers::init() -> void {
   auto messagePublisher = Services::messagePublisher();
@@ -27,27 +29,20 @@ auto Minebombers::init() -> void {
 
   auto rootNode = std::make_shared<Node<Transform3D>>("world");
 
-  auto terrainNode = std::make_shared<Node<Transform3D>>("terrain");
-  terrainNode->addAttachment(std::make_shared<SpriteAttachment>("test-ground/lava0"));
+  int seed = 4;
 
-  auto terrainNode1 = std::make_shared<Node<Transform3D>>("terrain");
-  terrainNode1->addAttachment(std::make_shared<SpriteAttachment>("test-ground/lava1"));
-  terrainNode1->setLocalPosition(glm::vec3(1, 0, 0));
+  auto random = StdLibRandom();
+  random.seed(seed);
 
-  auto terrainNode2 = std::make_shared<Node<Transform3D>>("terrain");
-  terrainNode2->addAttachment(std::make_shared<SpriteAttachment>("test-ground/lava2"));
-  terrainNode2->setLocalPosition(glm::vec3(0, 2, 0));
+  auto caveGen = CaveGenerator(seed, 64, 64, 4, 3);
+  auto tileMap = caveGen.generate();
+  auto levelCompiler = LevelCompiler(random);
 
-  auto terrainNode3 = std::make_shared<Node<Transform3D>>("terrain");
-  terrainNode3->addAttachment(std::make_shared<SpriteAttachment>("test-ground/lava3"));
-  terrainNode3->setLocalPosition(glm::vec3(-1, -1, 0));
-
-  rootNode->addChild(terrainNode);
-  rootNode->addChild(terrainNode1);
-  rootNode->addChild(terrainNode2);
-  rootNode->addChild(terrainNode3);
+  rootNode->addChild(levelCompiler.materializeGround(tileMap));
+  rootNode->addChild(levelCompiler.materializeObjects(tileMap));
 
   auto cameraNode = std::make_shared<Node<Transform3D>>("camera");
+  cameraNode->setLocalPosition(glm::vec3(12, 11, 0));
   rootNode->addChild(cameraNode);
 
   auto scene = std::make_shared<GameScene<Transform3D>>("gameScene", rootNode);
