1440826515e1b958eb9095c69a8f52d85b24ef54
Juuso Toikka <jtoikka@gmail.com>
2016-12-03 16:20:38 +0200

    :sparkles: Speed up rendering

--- include/graphics/Renderer.hpp
+++ include/graphics/Renderer.hpp
@@ -48,6 +48,9 @@ public:
     auto rootNode = sceneView.rootNode();
     auto viewport = sceneView.viewport();
 
+    _windowSize = _window.getSize();
+    _cameraPosition = cameraNode->position();
+
     // auto nodes = _getAllNodes(rootNode);
 
     // for (auto& node : nodes) {
@@ -64,6 +67,8 @@ private:
   int _tilesize;
   SpriteBatch _spriteBatch;
   KBMap<std::string, sf::Texture> textures;
+  glm::vec3 _cameraPosition;
+  sf::Vector2<uint> _windowSize;
 
   auto _getAllNodes(std::shared_ptr<Node<Transform3D>> node) -> KBVector<std::shared_ptr<Node<Transform3D>>> {
     auto v = KBVector<std::shared_ptr<Node<Transform3D>>>();
@@ -78,44 +83,56 @@ private:
     return v;
   }
 
+  auto _isWithinWindow(const glm::vec3& nodePosition) -> bool {
+    const auto& relativePosition = (nodePosition - _cameraPosition) * (float)_tilesize;
+    auto halfWidth = _windowSize.x / 2.0f;
+    auto halfHeight = _windowSize.y / 2.0f;
+
+    return
+      relativePosition.x < halfWidth &&
+      relativePosition.x > -halfWidth &&
+      relativePosition.y < halfHeight &&
+      relativePosition.y > -halfHeight;
+  }
+
   auto _renderNode(const std::shared_ptr<Node<Transform3D>> node, const std::shared_ptr<Node<Transform3D>> cameraNode, const Atlas& atlas) -> void {
-    const auto& resourceManager = Services::resourceManager();
-    auto spriteAttachment = node->get<SpriteAttachment>();
+    const auto& nodePosition = node->position();
 
-    spriteAttachment.foreach([&](auto s) {
-      auto id = s.spriteId();
-      const auto& someSprite = atlas.get(id);
-      if (someSprite.isDefined()) {
-        auto sprite = someSprite.get();
-        auto texPath = sprite.texturePath();
-        auto xy = sprite.xy();
-        auto spriteSize = sprite.size();
+    if (_isWithinWindow(nodePosition)) {
+      const auto& spriteAttachment = node->get<SpriteAttachment>();
+      spriteAttachment.foreach([&](auto s) {
+        auto id = s.spriteId();
+        const auto& someSprite = atlas.get(id);
+        if (someSprite.isDefined()) {
+          const auto& sprite = someSprite.get();
+          const auto& texPath = sprite.texturePath();
+          const auto& xy = sprite.xy();
+          const auto& spriteSize = sprite.size();
 
-        if (!textures.contains(texPath)) {
-          textures.insert(texPath, resourceManager->getRequired<Texture>(texPath)->getTexture());
+          if (!textures.contains(texPath)) {
+            const auto& resourceManager = Services::resourceManager();
+            textures.insert(texPath, resourceManager->getRequired<Texture>(texPath)->getTexture());
+          }
+          const auto& tex =textures[texPath];
+          sf::Sprite sfSprite(tex);
+          sfSprite.setTextureRect(sf::IntRect(
+            xy.x,
+            xy.y,
+            spriteSize.x,
+            spriteSize.y
+          ));
+
+          auto relativePosition = (nodePosition - _cameraPosition) * (float)_tilesize;
+
+          sfSprite.move(relativePosition.x + _windowSize.x / 2, -relativePosition.y + _windowSize.y / 2);
+
+          _window.draw(sfSprite);
+        } else {
+          throw ResourceException("Atlas does not contain sprite with id: " + id);
         }
-        const auto& tex =textures[texPath];
-        sf::Sprite sfSprite(tex);
-        sfSprite.setTextureRect(sf::IntRect(
-          xy.x,
-          xy.y,
-          spriteSize.x,
-          spriteSize.y
-        ));
+      });
+    }
 
-        const auto& nodePosition = node->position();
-        const auto& camPosition = cameraNode->position();
-        const auto& windowSize = _window.getSize();
-
-        auto relativePosition = (nodePosition - camPosition) * (float)_tilesize;
-
-        sfSprite.move(relativePosition.x + windowSize.x / 2, -relativePosition.y + windowSize.y / 2);
-
-        _window.draw(sfSprite);
-      } else {
-        throw ResourceException("Atlas does not contain sprite with id: " + id);
-      }
-    });
 
 
     auto children = node->children();
